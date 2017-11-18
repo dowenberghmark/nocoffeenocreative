e4e9bd67dc19d527dfdde4cc860b54aecead4fcc
Adam Wendelin <adwe@live.se>
2017-11-18 12:14:58 +0100

    working DonaldGen class

--- donald_generator.py
+++ donald_generator.py
@@ -5,31 +5,33 @@ import nltk
 import re
 nltk.download('averaged_perceptron_tagger')
 
-
 from nltk.tokenize import TweetTokenizer
 
 
 class POSifiedText(markovify.Text):
 
-    # Example from https://github.com/jsvine/markovify
+        # Example from https://github.com/jsvine/markovify
     #def word_split(self, sentence):
     #    words = re.split(self.word_split_pattern, sentence)
     #    words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
     #    return words
-#
-#    # Example from https://github.com/jsvine/markovify
-#    def word_join(self, words):
-#        sentence = " ".join(word.split("::")[0] for word in words)
-#        return sentence
+    #
+    #    # Example from https://github.com/jsvine/markovify
+    #    def word_join(self, words):
+    #        sentence = " ".join(word.split("::")[0] for word in words)
+    #        return sentence
+
+    def set_user_words(self, user_words):
+        self.user_words = user_words
 
     def test_sentence_output(self, words, max_overlap_ratio, max_overlap_total):
         """
-        Given a generated list of words, accept or reject it.
+            Given a generated list of words, accept or reject it.
 
-        Matches with overlap (built-in) and by matching to a list of user-strings (extended).
+            Matches with overlap (built-in) and by matching to a list of user-strings (extended).
         """
         l_words = [s.lower() for s in words]
-        l_userwords = [s.lower() for s in user_words]
+        l_userwords = [s.lower() for s in self.user_words]
 
         if set(l_words).intersection(l_userwords):
             return markovify.Text.test_sentence_output(self, words, max_overlap_ratio, max_overlap_total)
@@ -37,61 +39,58 @@ class POSifiedText(markovify.Text):
             return False
 
 
-def build_model(filename):
+class DonaldGen():
 
-    # Get raw text as string.
-    with open(filename) as f:
-        text = f.read()
+    def __init__(self, filename="lorem.txt"):
+        self.model = self.__build_model(filename)
 
-    # Build the model.
-    text_model = POSifiedText(text)
+    def __build_model(self, filename):
 
-    return text_model
+        # Get raw text as string.
+        with open(filename) as f:
+            text = f.read()
 
+        # Build the model.
+        text_model = POSifiedText(text)
 
-def noun_finder(sentence):
+        return text_model
 
-    tknzr = TweetTokenizer()
-    text = tknzr.tokenize(sentence)
+    def __noun_finder(self, sentence):
 
-    words = ["::".join(tag) for tag in nltk.pos_tag(text) ]
+        tknzr = TweetTokenizer()
+        text = tknzr.tokenize(sentence)
 
-    pattern = r'(.*)\::NN'
+        words = ["::".join(tag) for tag in nltk.pos_tag(text) ]
 
-    nouns = [w for w in words if 'NN' in w]
+        pattern = r'(.*)\::NN'
 
-    just_words = []
-    for w in words:
-        match = re.match(pattern, w)
-        if match:
-            just_words.append(match.group(1))
+        nouns = [w for w in words if 'NN' in w]
 
-    return just_words
+        just_words = []
+        for w in words:
+            match = re.match(pattern, w)
+            if match:
+                just_words.append(match.group(1))
 
+        return just_words
 
-def print_examples(text_model):
+    def print_examples(self):
 
-    # Print five randomly-generated sentences
-    for i in range(5):
-        print(text_model.make_sentence())
+        # Print five randomly-generated sentences
+        for i in range(5):
+            print(self.model.make_sentence())
 
+        # Print three randomly-generated sentences of no more than 140 characters
+        for i in range(3):
+            print(self.model.make_short_sentence(140))
 
-    # Print three randomly-generated sentences of no more than 140 characters
-    for i in range(3):
-        print(text_model.make_short_sentence(140))
-
-
+    def reply(self, user_says):
+        user_words = self.__noun_finder(user_says)
+        self.model.set_user_words(user_words)
+        return self.model.make_short_sentence(140)
 
 if __name__ == '__main__':
 
-
-    model = build_model("lorem.txt")
-
-    #print_examples(model)
-
-    sentence = "Put big game trophy decision on hold until such time as I review all conservation facts. Under study for years. Will update soon with Secretary Zinke. Thank you!"
-
-    user_words = noun_finder(sentence)
-
-    print_examples(model)
-    # print(user_words)
+    donald = DonaldGen()
+    s = donald.reply("lorem ipsum")
+    print(s)
